b664f243eafdf6e7c7f052b8bb98cfd2fe852ad3
enriquemore587 <jose.vergara2104@hotmail.com>
2019-05-29 17:41:53 -0500

    SESSION 6 | ListView & STYLE

--- app/src/main/java/com/enriquemore587/myapplication/MainActivity.java
+++ app/src/main/java/com/enriquemore587/myapplication/MainActivity.java
@@ -3,50 +3,48 @@ package com.enriquemore587.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner;
-    private EditText et1, et2;
-    private TextView tv;
-
-    private String[] opciones = new String[3];
+    private TextView textView;
+    private ListView listView;
+    private String[] nombres = new String[5];
+    private String[] edades = new String[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.nombres[0] = "JOSE";
+        this.edades[0] = "12";
 
-        this.et1 = (EditText) findViewById(R.id.TXT_PRIMER_VALOR);
-        this.et2 = (EditText) findViewById(R.id.TXT_SEGUNDO_VALOR);
-        this.tv = (TextView) findViewById(R.id.TXT_MESSAGE);
-        this.spinner = (Spinner) findViewById(R.id.spinner1);
-        this.opciones[0] = "SUMAR";
-        this.opciones[1] = "RESTAR";
-        this.opciones[2] = "MULTIPLICAR";
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_enriquemore587, opciones);
+        this.nombres[1] = "ENRIQUE";
+        this.edades[1] = "14";
 
-        this.spinner.setAdapter(adapter);
-    }
+        this.nombres[2] = "LUZ";
+        this.edades[2] = "20";
 
-    public void calcular(View view) {
-        Integer valor1 = Integer.parseInt(this.et1.getText().toString());
-        Integer valor2 = Integer.parseInt(this.et2.getText().toString());
-        String itemSelected = this.spinner.getSelectedItem().toString();
-        if (itemSelected.equals(this.opciones[0])) {
-            Integer resultado = valor1 + valor2;
-            this.tv.setText(String.format("Resultado: %s", String.valueOf(resultado)));
-        } else if (itemSelected.equals(this.opciones[1])) {
-            Integer resultado = valor1 - valor2;
-            this.tv.setText(String.format("Resultado: %s", String.valueOf(resultado)));
-        } else if (itemSelected.equals(this.opciones[2])) {
-            Integer resultado = valor1 * valor2;
-            this.tv.setText(String.format("Resultado: %s", String.valueOf(resultado)));
-        }
+        this.nombres[3] = "JORGE";
+        this.edades[3] = "22";
+
+        this.nombres[4] = "MARIANA";
+        this.edades[4] = "24";
+
+        this.textView = (TextView) findViewById(R.id.tv1);
+        this.listView = (ListView) findViewById(R.id.lb1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_enriquemore587, this.nombres);
+        this.listView.setAdapter(adapter);
+        this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(String.format("Usuario %s, tiene: %s", listView.getItemAtPosition(position), edades[position]));
+            }
+        });
     }
 
 }
